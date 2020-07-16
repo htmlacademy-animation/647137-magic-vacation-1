18caf7d87f0e1f644cb259e4b8784f1c850a1533
Papa <bitbtn@yandex.ru>
2020-07-16 14:16:23 +0300

    module1-task2

--- source/js/script.js
+++ source/js/script.js
@@ -21,3 +21,7 @@ social();
 
 const fullPageScroll = new FullPageScroll();
 fullPageScroll.init();
+
+window.addEventListener('load', (event) => {
+    document.querySelector('body').classList.add('loaded')
+})
