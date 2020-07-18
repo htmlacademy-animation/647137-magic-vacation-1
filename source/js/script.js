b636e0d4a8aab8c9b0995ac11a581e3c7ed60964
Papa <bitbtn@yandex.ru>
2020-07-18 14:11:13 +0300

    module2-task2

--- source/js/script.js
+++ source/js/script.js
@@ -8,7 +8,8 @@ import result from './modules/result.js';
 import form from './modules/form.js';
 import social from './modules/social.js';
 import FullPageScroll from './modules/full-page-scroll';
-import rules from './modules/rules.js'
+import rules from './modules/rules.js';
+import AccentTypographyBuild from './modules/fly-words.js'
 
 // init modules
 mobileHeight();
@@ -24,7 +25,17 @@ rules();
 const fullPageScroll = new FullPageScroll();
 fullPageScroll.init();
 
-
 window.addEventListener('load', (event) => {
     document.querySelector('body').classList.add('loaded')
 })
+
+const animationIntroText = new AccentTypographyBuild(`.text`, 600, `active`, `transform`);
+const animationDateText = new AccentTypographyBuild(`.date-text`, 600, `active`, `transform`);
+
+setTimeout(()=>{   
+    animationIntroText.runAnimation();
+}, 600);
+
+setTimeout(()=>{   
+    animationDateText.runAnimation();
+}, 1600);
